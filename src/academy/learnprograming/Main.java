01bcf7000b36a57fed2490612d872aed06418033
xtreme6007 <prestonnichols96@gmail.com>
2020-12-29 12:36:56 -0600

    Learning Java

---
+++ src/academy/learnprograming/Main.java
@@ -0,0 +1,19 @@
+package academy.learnprograming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+
+        System.out.println("Float Maximum Value:" + myMaxFloatValue);
+        System.out.println("Float Minimum Value:" + myMinFloatValue);
+
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+
+        System.out.println("Double Maximum Value:" + myMaxDoubleValue);
+        System.out.println("Double Minimum Value:" + myMinDoubleValue);
+    }
+}
